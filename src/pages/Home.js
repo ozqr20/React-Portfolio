d3d08c3fb3766cf9c068ee4c235a0ba8f6560403
Oscar <53874145+ozqr20@users.noreply.github.com>
2022-10-28 10:35:34 -0400

    home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,7 +2,57 @@ import React from 'react'
 
 function Home() {
   return (
-    <div>Home</div>
+    <div className='home'>
+      <div className='introduction'>
+        <h2>Hi, My Name is Oscar </h2>
+          <div className='me'>
+            <p> I am a junior programmer who graduated from college with an associate's 
+            degree in computer science and finished a bootcamp in full stack web development. I like challenges and consider myself a 
+            goal achiever. I like bowling, hike in the mountains, extreme sports, anime, and also I enjoyed volunteer for any non-profit
+            organization. </p> 
+          </div>
+      </div>
+      <div className='skills'>
+        <h1>Skills</h1>
+        <div className='front-end'>
+          <ol className='list'>
+            <li className='item'>
+              <h2>Front-End</h2>
+              <span>BootStrap</span><br></br>
+              <span>Bulma</span><br></br>
+              <span>React.js</span><br></br>
+              <span>HTMLL</span><br></br>
+              <span>CSS</span><br></br>
+              <span>NPM</span><br></br>
+              <span>React</span><br></br>              
+            </li>
+          </ol>
+        </div>
+        <div className='back-end'>
+          <ol className='list'>
+            <li className='item'>
+              <h2>Back-End</h2>
+              <span>Express.js</span><br></br>
+              <span>Node.js</span><br></br>
+              <span>MySQL</span><br></br>
+              <span>SQL</span><br></br>
+              <span>Heroku</span><br></br>
+              <span>MondoDB</span><br></br>
+            </li>
+          </ol>
+        </div>
+        <div className='languages'>
+          <ol className='list'>
+            <li className='item'>
+              <h2>Languages</h2>
+              <span>JavaScript</span><br></br>
+              <span>C++</span><br></br>
+              <span>Java</span><br></br>
+            </li>
+          </ol>
+        </div>
+      </div>
+    </div>
   )
 }
 
