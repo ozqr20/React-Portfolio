15d703660db1ae7720380940e4983b7861df84c6
Oscar <53874145+ozqr20@users.noreply.github.com>
2022-10-28 09:39:00 -0400

    css footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,14 +2,15 @@ import React from 'react'
 import GitHubIcon from '@material-ui/icons/GitHub';
 import  TwitterIcon  from '@material-ui/icons/Twitter';
 import  LinkedInIcon  from '@material-ui/icons/LinkedIn';
+import "../styles/Footer.css"
 
 function Footer() {
   return (
     <div className='footer'>
     <div className='socialLinks'>
-        < GitHubIcon />
-        < TwitterIcon />
-        < LinkedInIcon />
+        < GitHubIcon className='git'/>
+        < TwitterIcon className='twitt'/>
+        < LinkedInIcon className='linkedIn'/>
     </div>
     </div>
   )
